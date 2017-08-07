c156bf073901610460629116ff7c3891658268b7
sujit <sujit@192.168.0.16>
2017-08-07 23:03:30 +0200

    changes made

--- my.test.com/src/test/java/sujit/com/test/TestCheckBoxOptional.java
+++ my.test.com/src/test/java/sujit/com/test/TestCheckBoxOptional.java
@@ -20,19 +20,21 @@ public class TestCheckBoxOptional {
 	public static WebDriver driver;
 	public WebElement search;
 	Utility instance= new Utility();
-	
-	//Initialize the web browser
-@Test
-public void OpenBrowser() throws Throwable{
-	System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver");
-	
-	 driver= new FirefoxDriver();
-	 driver.manage().timeouts().implicitlyWait(300, TimeUnit.SECONDS);
-	 driver.get("https://live.runmyprocess.com/pub/621758689/appli/25209?P_mode=LIVE&P_language=fr");
-	 Thread.sleep(3000);
-}
-// The get the font size of the checkbox
 
+	//Initialize the web browser
+	@Test
+	public void OpenBrowser() throws Throwable{
+		String path = System.getProperty("src/main/resources/geckodriver.exe");
+		System.out.println("Path is "+ path);
+		System.setProperty("webdriver.gecko.driver", path);
+		
+		driver= new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(300, TimeUnit.SECONDS);
+		driver.get("https://live.runmyprocess.com/pub/621758689/appli/25209?P_mode=LIVE&P_language=fr");
+		Thread.sleep(3000);
+	}
+	// The get the font size of the checkbox
+	/*
 @Test(priority=1,dependsOnMethods= {"OpenBrowser"})
 public void getFontSize(){
 	search=driver.findElement(By.cssSelector("#id_informed_of_updates_case_studies_webinar_event_label"));
@@ -47,17 +49,17 @@ public void getFontFamily(){
 	String actualFontFamily= search.getCssValue("font-family");
 	//System.out.println(actualFontFamily);
 	Assert.assertEquals(actualFontFamily,instance.expectedFontFamily,"font-family not Match");
-}
+}*/
 
 
 
 
-@AfterClass
-public void closingbrowser(){
-	driver.close();
-	driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
-	
-}
+	@AfterClass
+	public void closingbrowser(){
+		driver.close();
+		driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
+
+	}
 
 }
 
